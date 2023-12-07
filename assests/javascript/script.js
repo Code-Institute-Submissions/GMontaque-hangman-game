5df982b0f1671dd22a61703988fe44f311933144
Giulio Montaque <giuliom@hotmail.co.uk>
2023-12-07 10:18:03 +0000

    create canvas with hangman drawing

--- assests/javascript/script.js
+++ assests/javascript/script.js
@@ -48,6 +48,10 @@ function stopReset() {
 	// remove disable off word length drop down
 	let wordNumLength = document.getElementById("guess-word");
 	wordNumLength.removeAttribute("disabled");
+	// reset canvas
+	const canvas = document.getElementById("hangman");
+	const context = canvas.getContext("2d");
+	context.clearRect(0, 0, canvas.width, canvas.height);
 }
 
 function startTimer() {
@@ -169,6 +173,67 @@ function winGame() {
 
 // fucntion reduce lives counter on wrong guess
 function reduceLives(lives) {
+	const canvas = document.getElementById("hangman");
+	const context = canvas.getContext("2d");
+
+	switch (parseInt(lives)) {
+		case 7:
+			context.strokeStyle = "#444";
+			context.lineWidth = 10;
+			context.beginPath();
+			context.moveTo(175, 225);
+			context.lineTo(5, 225);
+			context.moveTo(40, 225);
+			context.lineTo(25, 5);
+			context.lineTo(100, 5);
+			context.lineTo(100, 25);
+			context.stroke();
+			break;
+
+		case 6:
+			context.lineWidth = 5;
+			context.beginPath();
+			context.arc(100, 50, 25, 0, Math.PI * 2, true);
+			context.closePath();
+			context.stroke();
+			break;
+
+		case 5:
+			context.beginPath();
+			context.moveTo(100, 75);
+			context.lineTo(100, 140);
+			context.stroke();
+			break;
+
+		case 4:
+			context.beginPath();
+			context.moveTo(100, 85);
+			context.lineTo(60, 100);
+			context.stroke();
+			break;
+
+		case 3:
+			context.beginPath();
+			context.moveTo(100, 85);
+			context.lineTo(140, 100);
+			context.stroke();
+			break;
+
+		case 2:
+			context.beginPath();
+			context.moveTo(100, 140);
+			context.lineTo(80, 190);
+			context.stroke();
+			break;
+
+		case 1:
+			context.beginPath();
+			context.moveTo(100, 140);
+			context.lineTo(125, 190);
+			context.stroke();
+			break;
+	}
+
 	if (lives == 1) {
 		document.getElementById("gameLives").innerHTML = lives - 1;
 		looseGame();
